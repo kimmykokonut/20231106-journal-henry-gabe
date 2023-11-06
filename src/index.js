4563996902d40dc83b5f884feb7298157ab2ac10
Kim Robinson <kimmykokonut@gmail.com>
2023-11-06 11:58:35 -0800

    establish dev environment
    Co-authored-by: Henry Oberholtzer <henryoberholtzer@gmail.com>
    Co-authored-by: Gabriel Tucker <gabrieltucker260@gmail.com>

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import Journal from './../src/journal.js';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+
+const journal = new Journal;
---
+++ src/journal.js
@@ -0,0 +1,20 @@
+export default function Journal(title, entry) {
+    this.title = title;
+    this.entry = entry;
+}
+
+Journal.prototype.wordCount = function () {
+    return "I can't answer that yet!";
+
+};
+Journal.prototype.vowelCount = function () {
+    return "I can't answer that yet!";
+
+};
+Journal.prototype.consCount = function () {
+    return "I can't answer that yet!";
+
+};
+Journal.prototype.getTeaser = function () {
+    return "I can't answer that yet!";
+};
