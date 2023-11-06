65302c83c800275f2b40864c4a431bdbd39860a0
Kim Robinson <kimmykokonut@gmail.com>
2023-11-06 14:20:02 -0800

    print word count result to DOM
    Co-authored-by: Henry Oberholtzer <henryoberholtzer@gmail.com>
    Co-authored-by: Gabriel Tucker <gabrieltucker260@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
-import Journal from './../src/journal.js';
+import Entry from './../src/journal.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-const journal = new Journal;
+
+const form = document.querySelector('form#entry-form');
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const titleInput = document.getElementById('title').value;
+    const journalInput = document.getElementById('journal-body').value;
+    const entryInput = new Entry(titleInput, journalInput);
+    const entryWordCount = entryInput.wordCount();
+    // const entryVowelCount = entryInput.vowelCount();
+    // const entryConsCount = entryInput.consCount();
+
+    const divResponse = document.getElementById('response');
+    const pWordCount = document.createElement('p');
+    console.log(entryWordCount);
+    pWordCount.append("Number of words in your entry: " + entryWordCount);
+    console.log(pWordCount);
+    divResponse.append(pWordCount);
+});
+
